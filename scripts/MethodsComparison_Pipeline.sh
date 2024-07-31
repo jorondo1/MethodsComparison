7106d862d1020cdd7bb5509d5f197b70b8ec7089
Jonathan Rondeau-Leclaire <97983060+jorondo1@users.noreply.github.com>
2024-07-31 16:52:08 -0400

    output checks

--- scripts/MethodsComparison_Pipeline.sh
+++ scripts/MethodsComparison_Pipeline.sh
@@ -13,24 +13,28 @@ cd $MC
 export SALIVA_TSV=$PR19/Saliva/preproc/preprocessed_reads.sample.tsv
 export FECES_TSV=$PR19/Feces/preproc/preprocessed_reads.sample.tsv
 export MOSS_TSV=$MOSS/preproc/preprocessed_reads.sample.tsv
-export NUM_SALIVA=$(wc $SALIVA_TSV | awk '{print $1}')
-export NUM_FECES=$(wc $FECES_TSV | awk '{print $1}')
-export NUM_MOSS=$(wc $MOSS_TSV | awk '{print $1}')
+export NUM_Saliva=$(wc $SALIVA_TSV | awk '{print $1}')
+export NUM_Feces=$(wc $FECES_TSV | awk '{print $1}')
+export NUM_Moss=$(wc $MOSS_TSV | awk '{print $1}')
 
 ############
 # mOTUs ####
 # Custom SLURM script 
-sbatch --array=1-"$NUM_SALIVA" $MC/scripts/motus_SLURM.sh Saliva $SALIVA_TSV
-sbatch --array=1-"$NUM_FECES" $MC/scripts/motus_SLURM.sh Feces $FECES_TSV
-sbatch --array=1-"$NUM_MOSS" $MC/scripts/motus_SLURM.sh Moss $MOSS_TSV
+sbatch --array=1-"$NUM_Saliva" $MC/scripts/motus_SLURM.sh Saliva $SALIVA_TSV
+sbatch --array=1-"$NUM_Feces" $MC/scripts/motus_SLURM.sh Feces $FECES_TSV
+sbatch --array=1-"$NUM_Moss" $MC/scripts/motus_SLURM.sh Moss $MOSS_TSV
 
-ls */mOTU_abund/*_profile.txt | wc -l
+for i in Saliva Feces Moss; do
+	eval exp=\$NUM_$i
+	num=$(ls $i/mOTU_abund/*_profile.txt | wc -l)
+	echo "$num mOTUs output for $i found, $exp expected."
+done
 
 ####################
 # Kraken/bracken ###
 bracken="bash $ILL_PIPELINES/generateslurm_taxonomic_profile.sample.sh \
 	--kraken_db $ILAFORES/ref_dbs/kraken2_dbs/kraken2_PlusPFP_202202 \
-	--slurm_log $MC/logs --slurm_walltime 24:00:00 --slurm_threads 48 --slurm_mem 250G"
+	--slurm_log $MC/logs --slurm_walltime 72:00:00 --slurm_threads 48 --slurm_mem 250G"
 
 # Generate SLURM scripts https://github.com/jflucier/ILL_pipelines/blob/main/generateslurm_taxonomic_profile.sample.sh
 $bracken --sample_tsv $SALIVA_TSV --out $MC/Saliva/Bracken
@@ -38,11 +42,15 @@ $bracken --sample_tsv $FECES_TSV --out $MC/Feces/Bracken
 $bracken --sample_tsv $MOSS_TSV --out $MC/Moss/Bracken
 
 # Submit
-sbatch --array=1-"$NUM_SALIVA" $MC/Saliva/Bracken/taxonomic_profile.samples.slurm.sh
-sbatch --array=1-"$NUM_FECES" $MC/Feces/Bracken/taxonomic_profile.samples.slurm.sh
-sbatch --array=1-"$NUM_MOSS" $MC/Moss/Bracken/taxonomic_profile.samples.slurm.sh
+sbatch --array=1-"$NUM_Saliva" $MC/Saliva/Bracken/taxonomic_profile.samples.slurm.sh
+sbatch --array=1-"$NUM_Feces" $MC/Feces/Bracken/taxonomic_profile.samples.slurm.sh
+sbatch --array=1-"$NUM_Moss" $MC/Moss/Bracken/taxonomic_profile.samples.slurm.sh
 
-ls */Bracken/*/*-bugs_list.MPA.TXT | wc -l
+for i in Saliva Feces Moss; do
+	eval exp=\$NUM_$i
+	num=$(ls $i/Bracken/*/*_bracken/*_bracken_S.MPA.TXT | wc -l)
+	echo "$num Bracken output for $i found, $exp expected."
+done
 
 ################
 # MetaPhlAn4 ###
@@ -55,34 +63,46 @@ $metaphlan --sample_tsv $SALIVA_TSV --db $FAST/metaphlan4_db/mpa_vOct22_CHOCOPhl
 $metaphlan --sample_tsv $FECES_TSV --db $FAST/metaphlan4_db/mpa_vOct22_CHOCOPhlAnSGB_202212 --out $MC/Feces/MPA_db2022
 $metaphlan --sample_tsv $MOSS_TSV --db $FAST/metaphlan4_db/mpa_vOct22_CHOCOPhlAnSGB_202212 --out $MC/Moss/MPA_db2022
 
-sbatch --array=1-"$NUM_SALIVA" $MC/Saliva/MPA_db2022/metaphlan.slurm.sh
-sbatch --array=1-"$NUM_FECES" $MC/Feces/MPA_db2022/metaphlan.slurm.sh
-sbatch --array=1-"$NUM_MOSS" $MC/Moss/MPA_db2022/metaphlan.slurm.sh
+sbatch --array=1-"$NUM_Saliva" $MC/Saliva/MPA_db2022/metaphlan.slurm.sh
+sbatch --array=1-"$NUM_Feces" $MC/Feces/MPA_db2022/metaphlan.slurm.sh
+sbatch --array=1-"$NUM_Moss" $MC/Moss/MPA_db2022/metaphlan.slurm.sh
 
-ls */MPA_db2022/*/*_profile.txt | wc -l
 
 # 2023 database
 $metaphlan --sample_tsv $SALIVA_TSV --db $FAST/metaphlan4_db/mpa_vJun23_CHOCOPhlAnSGB_202307 --out $MC/Saliva/MPA_db2023
 $metaphlan --sample_tsv $FECES_TSV --db $FAST/metaphlan4_db/mpa_vJun23_CHOCOPhlAnSGB_202307 --out $MC/Feces/MPA_db2023
 $metaphlan --sample_tsv $MOSS_TSV --db $FAST/metaphlan4_db/mpa_vJun23_CHOCOPhlAnSGB_202307 --out $MC/Moss/MPA_db2023
 
-sbatch --array=1-"$NUM_SALIVA" $MC/Saliva/MPA_db2023/metaphlan.slurm.sh
-sbatch --array=1-"$NUM_FECES" $MC/Feces/MPA_db2023/metaphlan.slurm.sh
-sbatch --array=1-"$NUM_MOSS" $MC/Moss/MPA_db2023/metaphlan.slurm.sh
+sbatch --array=1-"$NUM_Saliva" $MC/Saliva/MPA_db2023/metaphlan.slurm.sh
+sbatch --array=1-"$NUM_Feces" $MC/Feces/MPA_db2023/metaphlan.slurm.sh
+sbatch --array=1-"$NUM_Moss" $MC/Moss/MPA_db2023/metaphlan.slurm.sh
 
-ls */MPA_db2023/*/*_profile.txt | wc -l
+for MPA in MPA_db2022 MPA_db2023; do
+for i in Saliva Feces Moss; do
+	eval exp=\$NUM_$i
+	num=$(ls $i/$MPA/*/*_profile.txt | wc -l)
+	echo "$num $MPA output for $i found, $exp expected."
+done
+done
 
 #####################
 # Sourmash gather ###
 
-sbatch --mem=60G --array=1-"$NUM_SALIVA" $MC/scripts/gather_SLURM.sh Saliva $SALIVA_TSV "genbank-2022.03"
-sbatch --mem=60G --array=1-"$NUM_FECES" $MC/scripts/gather_SLURM.sh Feces $FECES_TSV "genbank-2022.03"
-sbatch --mem=80G --array=1-"$NUM_MOSS" $MC/scripts/gather_SLURM.sh Moss $MOSS_TSV "genbank-2022.03"
+sbatch --mem=60G --array=1-"$NUM_Saliva" $MC/scripts/gather_SLURM.sh "Saliva" $SALIVA_TSV "genbank-2022.03"
+sbatch --mem=60G --array=1-"$NUM_Feces" $MC/scripts/gather_SLURM.sh "Feces" $FECES_TSV "genbank-2022.03"
+sbatch --mem=80G --array=1-"$NUM_Moss" $MC/scripts/gather_SLURM.sh "Moss" $MOSS_TSV "genbank-2022.03"
 
-sbatch --mem=15G --array=1-"$NUM_SALIVA" $MC/scripts/gather_SLURM.sh Saliva $SALIVA_TSV "gtdb-rs220"
-sbatch --mem=15G --array=1-"$NUM_FECES" $MC/scripts/gather_SLURM.sh Feces $FECES_TSV "gtdb-rs220"
-sbatch --mem=15G --array=1-"$NUM_MOSS" $MC/scripts/gather_SLURM.sh Moss $MOSS_TSV "gtdb-rs220"
+sbatch --mem=15G --array=1-"$NUM_Saliva" $MC/scripts/gather_SLURM.sh "Saliva" $SALIVA_TSV "gtdb-rs220"
+sbatch --mem=15G --array=1-"$NUM_Feces" $MC/scripts/gather_SLURM.sh "Feces" $FECES_TSV "gtdb-rs220"
+sbatch --mem=15G --array=1-"$NUM_Moss" $MC/scripts/gather_SLURM.sh "Moss" $MOSS_TSV "gtdb-rs220"
 
+for SM_db in genbank-2022.03 gtdb-rs220; do
+for i in Saliva Feces Moss; do
+	eval exp=\$NUM_$i
+	num=$(ls $i/Sourmash/*${SM_db}_gather.csv | wc -l)
+	echo "$num $SM_db output for $i found, $exp expected."
+done
+done
 
 
 
