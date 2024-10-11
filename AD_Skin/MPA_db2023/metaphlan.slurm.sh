cf706a1e79653151707655aa8775a1390623ab17
Jonathan Rondeau-Leclaire <ronj2303@ip34-rockylinux8.m>
2024-10-11 11:51:17 -0400

    AD_skins

---
+++ AD_Skin/MPA_db2023/metaphlan.slurm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+
+#SBATCH --mail-type=END,FAIL
+#SBATCH -D /nfs3_ib/nfs-ip34/home/def-ilafores/analysis/MethodsComparison/AD_Skin/MPA_db2023
+#SBATCH -o /nfs3_ib/nfs-ip34/home/def-ilafores/analysis/MethodsComparison/logs/metaphlan-%A_%a.slurm.out
+#SBATCH --time=24:00:00
+#SBATCH --mem=30G
+#SBATCH -N 1
+#SBATCH -n 24
+#SBATCH -A def-ilafores
+#SBATCH -J metaphlan
+
+
+echo "loading env"
+module load StdEnv/2020 apptainer/1.1.5
+
+export __sample_line=$(cat /nfs3_ib/nfs-ip34/home/def-ilafores/analysis/MethodsComparison/AD_Skin/preproc/preprocessed_reads.sample.tsv | awk "NR==$SLURM_ARRAY_TASK_ID")
+export __sample=$(echo -e "$__sample_line" | cut -f1)
+export __fastq_file1=$(echo -e "$__sample_line" | cut -f2)
+export __fastq_file2=$(echo -e "$__sample_line" | cut -f3)
+export __fastq_file1_single=$(echo -e "$__sample_line" | cut -f4)
+export __fastq_file2_single=$(echo -e "$__sample_line" | cut -f5)
+
+bash -l /nfs3_ib/nfs-ip34/home/def-ilafores/analysis/MethodsComparison/ILL_pipelines/scripts/taxonomic_abundance.metaphlan.sh \
+-o /nfs3_ib/nfs-ip34/home/def-ilafores/analysis/MethodsComparison/AD_Skin/MPA_db2023/ \
+-tmp $SLURM_TMPDIR \
+-t 24 \
+-s $__sample \
+-fq1 $__fastq_file1 \
+-fq2 $__fastq_file2 \
+-fq1_single $__fastq_file1_single \
+-fq2_single $__fastq_file2_single \
+-db /nfs3_ib/nfs-ip34/fast/def-ilafores/metaphlan4_db/mpa_vJun23_CHOCOPhlAnSGB_202307
+
+
