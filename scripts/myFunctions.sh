60065433eb5a163bf80f8daa429680b9d53dd2a5
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2024-10-11 09:47:31 -0400

    check function

---
+++ scripts/myFunctions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+check_output() {
+	if [ $# -lt 3 ]; then
+	        echo "!!Usage: my_function <'database1 database2 ... databaseN'> <DATASETS> <filename_suffix>"
+	        return 1  # Exit the function with a non-zero status
+	fi
+	filename_suffix="$3"
+	DATASETS="$2"
+	DATABASES="$1"
+
+	for db in $DATABASES; do
+		for ds in $DATASETS; do
+			TSV="${ds}_TSV"
+			eval exp=\$NUM_$ds
+			found=$(find $ds/$db -type f -name "*$filename_suffix" -exec basename {} \; | sed "s/${filename_suffix}//")
+			num=$(echo $found | wc -w)
+			
+			echo "$num $test output for $ds found, $exp expected."
+			if [ "$num" -lt "$exp" ]; then 
+			grep -nv -E "$found" $TSV | cut -d: -f1| paste -s -d,
+			fi
+		done
+	done
+}
+
