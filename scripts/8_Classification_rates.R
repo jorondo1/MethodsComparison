df6130542d1726b9f2b6558cd4b2d41422dea213
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-04-09 15:46:07 -0400

    plot with n

--- scripts/8_Classification_rates.R
+++ scripts/8_Classification_rates.R
@@ -1,7 +1,7 @@
 library(pacman)
 p_load(tidyverse)
 
-crate <- read_tsv('kraken_classification_rate.tsv', col_names = c('path','Rate')) %>% 
+class_rate <- read_tsv('kraken_classification_rate.tsv', col_names = c('path','Rate')) %>% 
   separate(path, into = c('dot','Dataset', 'Tool', 'Sample', 'Report' ),
            sep = '/') %>% 
   select(-dot, -Report) %>% 
@@ -11,10 +11,26 @@ crate <- read_tsv('kraken_classification_rate.tsv', col_names = c('path','Rate')
                               TRUE ~ 'RefSeq'),
          Tool = str_remove(Tool, '_GTDB'))
 
+# Subset samples with both GTDB and RefSeq
+sample_subset <- class_rate %>% 
+  group_by(Dataset, Sample) %>% 
+  summarise(n = n()) %>% 
+  filter(n == 6) # should have 6 entries per sample
 
-crate %>% 
+# Create a label for each dataset with n = sample_count
+Dataset_n_labels <- sample_subset %>% 
+  select(-Sample) %>% 
+  group_by(Dataset) %>% 
+  summarise(n = n()) %>% 
+  mutate(Dataset_n = paste0(Dataset, " (n = ", n, ")")) %>% 
+  select(-n)
+
+# Plot 
+class_rate %>% 
+  left_join(Dataset_n_labels, by = 'Dataset') %>% 
+  filter(Sample %in% pull(sample_subset, Sample)) %>% 
   ggplot(aes(x = Tool, y = Rate, fill = Database)) +
   geom_boxplot() +
-  facet_grid(~Dataset) +
+  facet_grid(~Dataset_n) +
   theme_light()
 
