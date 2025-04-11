ad64432b5429bc345609bfc8bd0fe007e96e5f3c
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-04-10 22:11:57 -0400

    anchor in singularity exec

--- scripts/gather_SLURM_fast.sh
+++ scripts/gather_SLURM_fast.sh
@@ -12,7 +12,7 @@ start_time=$(date +%s)
 
 export ILAFORES=$ANCHOR/$ILAFORES
 # Load sourmash
-export sourmash="singularity exec --writable-tmpfs -e -B ${ANCHOR}/home:${ANCHOR}/home ${ILAFORES}/programs/ILL_pipelines/containers/sourmash.4.8.11.sif sourmash"
+export sourmash="singularity exec --writable-tmpfs -e -B ${ANCHOR}/fast2/def-ilafores:${ANCHOR}/fast2/def-ilafores ${ILAFORES}/programs/ILL_pipelines/containers/sourmash.4.8.11.sif sourmash"
 module load StdEnv/2020 apptainer/1.1.5
 
 # Parse options
