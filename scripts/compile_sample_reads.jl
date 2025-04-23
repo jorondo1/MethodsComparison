75da0e55ef61972d94171a343e2d1bdd16b27726
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-04-23 11:49:37 -0400

    generate full report

--- scripts/compile_sample_reads.jl
+++ scripts/compile_sample_reads.jl
@@ -33,38 +33,63 @@ function parse_commandline()
 	
 end
 
-#= #...................................
-Adding workers for parallel processing.
-=# #...................................
-
 #= *************************
 Function Generate file list*
 =# #************************
-using Glob
+
 function generateFastaList(directories::Vector{String})
     fasta_files = String[]
     for dir in directories
         files = Glob.glob("*/*.fa*", dir)
-        for file in files
-            # Keep ONLY if:
-            # 1. Contains "_paired_" AND
-            # 2. Does NOT contain "contam", "_2.", or "unmatched"
+        for file in files # only keep 1st mate in pair, without contam files
             if occursin("_paired_", file) &&
                !(occursin("contam", file) || occursin("_2.", file) || occursin("unmatched", file))
                 push!(fasta_files, file)
             end
         end
     end
-    println(fasta_files)
+    return(fasta_files)
 end
 
-#= ******************
-Function Count reads*
-=# #*****************
+#= #...................................
+Adding workers for parallel processing.
+=# #...................................
 
-#= **********************
-Function Generate report*
-=# #*********************
+using Distributed, Gzip_jll, CodecZlib
+addprocs(args["n_cores"])  
+
+#= *******************
+Function Count reads *
+=# #******************
+@everywhere using Distributed, FASTX, DataFrames, CSV
+@everywhere begin
+    function count_reads_fastx(filename::String)
+        reader = FASTQ.Reader(open(filename))
+        count = 0
+        for _ in reader
+            count += 1
+        end
+        close(reader)
+        return count
+    end
+end
+
+#= ***********************
+Function generate report *
+=# #**********************
+function generate_read_counts(input_dirs::Vector{String}, output_path::String)
+    fastq_files = generateFastaList(input_dirs)  
+    
+    # Parallel counting
+    results = @distributed (vcat) for file in fastq_files
+    	(filepath = file, count = count_reads_fastx(file))
+    end
+
+    # Save as CSV
+    df = DataFrame(results)
+    CSV.write(output_path, df)
+    return df
+end
 
 #= 
 Main 
@@ -72,7 +97,8 @@ Main
 
 function main()
 	args = parse_commandline()
-	generateFastaList(args["input_directories"])
+	output_file = joinpath(args["output_dir"], "read_counts.csv")
+	generate_read_counts(args["input_directories"], output_file)
 end
 
 main()
