e6456833ce44c744d827a13c6b4a1662d17486e6
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2025-04-23 11:03:53 -0400

    check file listing

---
+++ scripts/compile_sample_reads.jl
@@ -0,0 +1,82 @@
+#!/usr/bin/env julia
+
+# Compile the number of reads in metagenome samples
+
+#= #............
+Parse arguments.
+=# #............
+using ArgParse
+
+function parse_commandline()
+	s = ArgParseSettings()
+    
+	@add_arg_table s begin
+		"--n_cores", "-c"
+			help = "Number of cores for parallel processing"
+			arg_type = Int
+			default = 2
+		"--output_dir", "-o"
+			help = "Path where reports will be saved"
+			arg_type = String
+			required = true
+		"--paired_only"
+			help = "Only count paired samples (will look for the string 'paired' in filenames)"
+			default = true
+		"input_directories"
+			help = "Input directories to process (at least one required)"
+			required = true  
+			nargs = '+'  # "+" means one or more arguments
+	end
+	
+	return parse_args(s)
+	
+end
+
+#= #...................................
+Adding workers for parallel processing.
+=# #...................................
+
+#= *************************
+Function Generate file list*
+=# #************************
+using Glob
+function generateFastaList(directories::Vector{String})
+	fasta_files = String[] # define empty vector
+	for dir in directories
+		# Find all files
+		files = Glob.glob("*", dir)
+		
+		for file in files
+			basename = lowercase(file)
+			if endswith(basename, ".gz")
+				basename = basename[1:end-3] #strip .gz
+			end
+			if (endswith(basename, ".fa") || endswith(basename, ".fasta")) && !occursin("contam", basename)
+			println(basename)
+			end
+		end
+	end	
+end
+
+#= ******************
+Function Count reads*
+=# #*****************
+
+#= **********************
+Function Generate report*
+=# #*********************
+
+#= 
+Main 
+=# 
+
+function main()
+	args = parse_commandline()
+    
+    println("Number of cores: $(args["n_cores"])")
+    println("Output directory: $(args["output_dir"])")
+    println("Paired only: $(args["paired_only"])")
+    println("Input directories: $(args["input_directories"])")
+end
+
+main()
