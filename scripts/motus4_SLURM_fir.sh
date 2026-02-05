893ec76af6aae9ba546770ac9ec386bf49a44357
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2026-02-05 12:50:54 -0500

    claudified

--- scripts/motus4_SLURM_fir.sh
+++ scripts/motus4_SLURM_fir.sh
@@ -2,9 +2,9 @@
 #SBATCH -D /project/def-ilafores/ronj2303/MethodsComparison
 #SBATCH -o /project/def-ilafores/ronj2303/MethodsComparison/logs/mOTU-%A_%a.slurm.out
 #SBATCH --time=24:00:00
-#SBATCH --mem=46G
+#SBATCH --mem=90G
 #SBATCH -N 1
-#SBATCH -c 48
+#SBATCH -c 24
 #SBATCH -A def-ilafores
 #SBATCH -J motus4
 
